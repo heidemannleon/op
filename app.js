88b08c1fa42e83b74ba98e618d73490a8db0f5e1
heidemannleon <leon.heidemann@khk.ee>
2019-11-15 13:48:48 +0200

    dokumendi struktuuri omadused

--- app.js
+++ app.js
@@ -1,53 +1,51 @@
+ // document meetodid
+ // node omadused ja meetodid
 
-let sisu = document.querySelector('#title');
-// // id järgi
- sisu = document.querySelector('.card-title');
+ let nimekiri = document.querySelectorAll('al');
+ ('ul.collection')
+ let nimekirjaElement = document.querySelectorAll('li.collection-item');
+ console.log(nimekiri);
+ console.log(nimekirjaElement);
 
- sisu = document.querySelector('h5'); // elemendi järgi 
+ nimekiri = document.querySelector('ul.collection');
+sisu = nimekiri.childNodes;
+sisu = nimekiri.childNodes[0];
+sisu = nimekiri.childNodes[0].nodeName;
+sisu = nimekiri.childNodes[5].nodeType;
+;
+/*
+1 - element
+2 - atribuut
+3 - tekst
+8 - kommentaar
+9 - doctype
+10 - doctype
+*/
+sisu = nimekiri.children;
+sisu = nimekiri.children[1];
+nimekiri.children[1].textContent = 'Õpi rohkem javascripti' 
+sisu = nimekiri.children[2].children[0].id = 'kaitse';
+console.log(sisu);
+console.log(nimekiri.children[2].children[0]);
 
-//stiili muutmine
-document.querySelector('li').style.color = 'red';
-document.querySelector('ul li').style.color = 'blue';
-document.querySelector('li:last-child').style.color = 'red';
-document.querySelector('li:nth-child(2)').style.color = 'orange';
+sisu = nimekiri.firstChild; // node type
+sisu = nimekiri.firstElementChild; // value
+
+sisu = nimekiri.childElementCount;
+
+nimekirjaElement = document.querySelector('li.collection-item');
+
+sisu = nimekirjaElement.parentNode;
+sisu = nimekirjaElement.parentElement;
+sisu = nimekirjaElement.parentElement.parentElement;
+
+
+sisu = nimekirjaElement.nextSibling; //node type
+
+sisu = nimekirjaElement.nextSibling; // value 
+
+sisu = nimekirjaElement.previousSibling; // mpde type
+sisu = 
 console.log(sisu);
 
-
- //multiply elements selectors
  
- sisu = document.getElementsByClassName('collection-item');
- console.log(sisu[0]) //[0] on index, laseb valida esimese elemendi Sisu seest.
- sisu[0].style.color = 'orange'; //teen esimese elemendi orange värvi 
- sisu[2].textContent = 'Õpi Arvutivõrgud'; // 
- let nimekirjaElemendid = document.querySelector('ul').getElementsByClassName('collection-item');
-
- // querySelector klassi nime järgi
-nimekirjaElemendid = document.querySelector('ul').getElementsByClassName('collection-item');
-console.log(nimekirjaElemendid);
-//elemendid nimejärgi
-nimekirjaElemendid = document.getElementsByTagName('li')
-
-// teisendame htmlCollection massiiviks (Array)
- nimekirjaElemendid = Array.from(nimekirjaElemendid);
- console.log(nimekirjaElemendid);
- // massiivi meetodite kasutamine
- nimekirjaElemendid.reverse();
- //massiivi läbimine tsükliga
- //for
- for(let i = 0; i < nimekirjaElemendid.length; i++){
-     nimekirjaElemendid[i].style.background = '#ccc';
-     nimekirjaElemendid[1].style.background = '#f4f4f4';
-
-     console.log(nimekirjaElemendid[i]);
- }
- /* forEach
- nimekirjaElemendid.forEach(function(Element){
-     console.log(element);
-     element.style.background = '#ddd';
- });
- */
-
- let nimekiriPaaritu = document.querySelectorAll('li:nth-child(odd)');
- console.log(nimekiriPaaritu);
- let nimekiriPaaris = document.querySelectorAll('li:nth-child(even)');
- console.log(nimekiriPaaritu);
