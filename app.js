db9f870670d24520911c6c9174d0c9546c5ee718
heidemannleon <leon.heidemann@khk.ee>
2019-11-20 11:27:33 +0200

    sündmused - sisse ja väljapoole elementidest suunamine

--- app.js
+++ app.js
@@ -4,40 +4,59 @@ const form = document.querySelector('form');
 const lisaUusYlesanne = document.getElementById('task');
 const text = document.querySelector('h5');
 
-//
+// kustutame lisUusYlesanne väärtus
 lisaUusYlesanne.value = '';
 
-//
-// form.addEventListener('submit', syndmus);
-// lisaUusYlesanne.addEventListener('focus',syndmus);
-// lisaUusYlesanne.addEventListener('blur',syndmus);
-
-// lisaUusYlesanne.addEventListener('paste',syndmus);
-// lisaUusYlesanne.addEventListener('cut',syndmus);
-
-// lisaUusYlesanne.addEventListener('input',syndmus);
-//
-
-
-
-
-
-
-
-
-
-
-
-
+// klaviatuur
+// keydown
+// lisaUusYlesanne.addEventListener('keydown', syndmus);
+// keyup
+// lisaUusYlesanne.addEventListener('keyup', syndmus);
+// keypress
+// lisaUusYlesanne.addEventListener('keypress', syndmus);
+// focus
+// lisaUusYlesanne.addEventListener('focus', syndmus);
+// blur
+// lisaUusYlesanne.addEventListener('blur', syndmus);
+// paste
+// lisaUusYlesanne.addEventListener('paste', syndmus);
+// cut
+// lisaUusYlesanne.addEventListener('cut', syndmus);
+// input
+// lisaUusYlesanne.addEventListener('input', syndmus);
 
 
 function syndmus(e){
-    // 
-    console.log(`Sündmuse tüüp: ${e.type}`);
-    //
-    console.log(lisaUusYlesanne.value);
-    //
-    text.innerText = lisaUusYlesanne.value;
-    
-    
+  // logi sündmuse tüüp
+  console.log(`Sündmuse tüüp: ${e.type}`);
+  // kontrollime vormi kaudu saadetud väärtus
+  console.log(lisaUusYlesanne.value);
+  // väljastame ülesanne HTML-is
+  text.innerText = lisaUusYlesanne.value;
+} 
+// bubbling
+
+// lehe kaardi pealkiri
+// document.querySelector('.card-title').addEventListener('click', function(e){
+//   console.log('card-title');
+// });
+
+// lehe kaardi sisu
+document.querySelector('.card-content').addEventListener('click', function(e){
+  console.log('card-content');
+});
+
+// delegeerimine
+
+const kustutaYlesanne = document.querySelector('.delete-item');
+kustutaYlesanne.addEventListener('click', kustuta);
+
+// console.log(kustutaYlesanne);
+
+function kustuta(e){
+   if(e.target.parentElement.classList.contains('delete-item')){
+    console.log('hakkame kustutama');
+    e.target.parentElement.parentElement.remove();
+  } 
 }
+
