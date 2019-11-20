5ef0aa4b0f17bf68f5c33e161603a7991d0b2984
heidemannleon <leon.heidemann@khk.ee>
2019-11-20 09:55:20 +0200

    sündmused - hiire sündmused

--- app.js
+++ app.js
@@ -22,7 +22,31 @@ function vajutaNupp(e){
   sisu = e.offsetY;
   sisu = e.offsetX;
   console.log(sisu);
-}
-
-
-
+const text = document.querySelector('h5');
+const kaart = document.querySelector('.card');
+// click
+// kustutaKoik.addEventListener('click', syndmus);
+// double click
+// kustutaKoik.addEventListener('dblclick', syndmus);
+// mouse down
+// kustutaKoik.addEventListener('mousedown', syndmus);
+// mouse up
+// kustutaKoik.addEventListener('mouseup', syndmus);
+// mouse enter
+// kaart.addEventListener('mouseenter', syndmus);
+// mouse leave
+// kaart.addEventListener('mouseleave', syndmus);
+// mouse over
+// kaart.addEventListener('mouseover', syndmus);
+// mouse out
+// kaart.addEventListener('mouseout', syndmus);
+// mouse move
+kaart.addEventListener('mousemove', syndmus);
+function syndmus(e){
+  // logi sündmuse tüüp
+  console.log(`Sündmuse tüüp: ${e.type}`);
+  // muuda text sisu: HiirX: x koord HiirY: y koord
+  text.textContent =  `HiirX: ${e.offsetX} HiirY: ${e.offsetY}`;
+  // koosta body värv rgb(Xkoord, Ykoord, 40);
+  document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 40)`;
+} 
