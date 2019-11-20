3ed547c262d7a9011ee46569bffd54a388b94b64
heidemannleon <leon.heidemann@khk.ee>
2019-11-20 09:25:34 +0200

    sündmused - sündmusobjekti omadused

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 // sündmused
 
-const kustutaKoik = document.querySelector('.clear-tasks')
-kustutaKoik.addEventListener('click', function(e){
-console.log('Vajutatud');
-// e.preventDefault();
-});
+const kustutaKoik = document.querySelector('.clear-tasks');
+kustutaKoik.addEventListener('click', vajutaNupp);
 
+function vajutaNupp(e){
+  let sisu = e;
+  // target
+  sisu = e.target;
+  sisu = e.target.id;
+  sisu = e.target.className;
+  sisu = e.target.classList;
 
+  // tüüp
+  sisu = e.type;
 
+  // sündmuse kordinaadid browseri akna suhtes
+  sisu = e.clientY;
+  sisu = e.clientX;
 
-
-
+  // sündmuse kordinaadid elemendi enda suhtes
+  sisu = e.offsetY;
+  sisu = e.offsetX;
+  console.log(sisu);
+}
 
 
 
