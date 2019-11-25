8fdc50628e1540453c30308963e57c27ae3126dd
heidemannleon <leon.heidemann@khk.ee>
2019-11-25 15:46:52 +0200

    Raamatud - teade väljastamine

--- app.js
+++ app.js
@@ -5,6 +5,49 @@ function Raamat(a, p, i){
     this.isbn = i;
   }
   
+  // Kasutaja liides - KL
+  // konstruktor
+  function KL() {
+  
+  }
+  
+  // KL funktsionaal
+  // sisendväljade puhastamine
+  KL.prototype.puhastaSisend = function(){
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+  }
+  
+  // raamatu lisamine tabelisse
+  KL.prototype.lisaRaamatTabelisse = function(r){
+    // loome tabeli rida
+    const rida = document.createElement('tr');
+    // täidame rida tabeli andmetega
+    rida.innerHTML = `
+      <td>${r.pealkiri}</td>
+      <td>${r.autor}</td>
+      <td>${r.isbn}</td>
+    `;
+    // lisame rida tabelisse
+    tabel = document.getElementById('book-list');
+    tabel.appendChild(rida);
+  }
+  
+  // teade väljastamine
+  KL.prototype.teade = function(s){
+    // loome div, kuhu lisada teade sõnum
+    const div = document.createElement('div');
+    // lisame sõnumi tekst divi sisse
+    const tekst = document.createTextNode(s);
+    div.appendChild(tekst);
+    // leiame elemendid, mille suhtes tuleb lisada uus element
+    const konteiner = document.querySelector('.container');
+    const vorm = document.getElementById('book-form');
+    // lisame teade dokumendi
+    konteiner.insertBefore(div, vorm);
+  }
+  
   // kirjeldame raamatu lisamise sündmust
   document.getElementById('book-form').addEventListener('submit', lisaRaamat);
   // raamatu lisamise funktsioon
@@ -14,9 +57,24 @@ function Raamat(a, p, i){
     const autor = document.getElementById('author').value;
     const isbn = document.getElementById('isbn').value;
     // loome raamat andmete põhjal
-    const raamat = new Raamat(pealkiri, autor, isbn);
+    const raamat = new Raamat(autor, pealkiri, isbn);
   
-    console.log(raamat);
+    // loome kasutaja liidese objekt temaga opereerimiseks
+    const kl = new KL();
+  
+    // kui mingid andmed on puudu, 
+    // siis anname märku
+    if(pealkiri == '' | autor == '' | isbn == ''){
+      kl.teade('Tuleb täita kõik väljad!');
+    } else {
+      // muidu
+      // lisame sisestatud raamat tabelisse
+      kl.lisaRaamatTabelisse(raamat);
+      kl.teade('Raamat on lisatud!');
+    }
+  
+    // puhastame väljad sisestatud andmetest
+    kl.puhastaSisend();
   
     e.preventDefault();
   }
