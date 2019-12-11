9728a164c89c82d865a2ccb71c6bf93b0ee0187c
heidemannleon <leon.heidemann@khk.ee>
2019-12-11 15:11:07 +0200

    Saadud ilmaandmete näitamine

---
+++ weatherApp/app.js
@@ -0,0 +1,12 @@
+  
+// weather for given location
+const weather = new Weather('Tartu linn');
+// app UI
+const ui = new UI();
+
+weather.weatherData().then(cityWeather => {
+  ui.print(cityWeather);
+}).catch(error => console.log(error));
+
+
+console.log(ui);
